e3b370ec0818a1edef97f659edb0df654910a47f
YongXun <13623048317@163.com>
2020-07-15 18:11:38 +0800

    恢复路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -42,12 +42,12 @@ const router = new VueRouter({
     routes
 })
 
-//挂载路由导航
-// router.beforeEach((to, from, next) => {
-//     if (to.path === '/login/signin' || to.path === '/login/register') return next();
-//     //获取token
-//     const tokenStr = window.sessionStorage.getItem('token');
-//     if (!tokenStr) { return next('/login'); } else { next(); }
-// })
+// 挂载路由导航
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login/signin' || to.path === '/login/register') return next();
+    //获取token
+    const tokenStr = window.sessionStorage.getItem('token');
+    if (!tokenStr) { return next('/login'); } else { next(); }
+})
 
 export default router
